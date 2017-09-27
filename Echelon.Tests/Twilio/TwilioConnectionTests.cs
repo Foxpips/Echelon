6fdbd2866205a17dda10704a3c1a9dd8c9132b15
Simon Markey <simon.markey@buy4now.ie>
2017-09-27 16:36:03 +0100

    fixing errors with package building from npm

--- Echelon.Tests/Twilio/TwilioConnectionTests.cs
+++ Echelon.Tests/Twilio/TwilioConnectionTests.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using NUnit.Framework;
-using Twilio.Auth;
-using Twilio.IpMessaging;
-using Twilio.IpMessaging.Model;
-
-namespace Echelon.Tests.Twilio
-{
-    public class TwilioConnectionTests
-    {
-        [Test]
-        [Ignore("Twilio issues")]
-        public void Method_Scenario_Result()
-        {
-            // Find your Account Sid and Auth Token at twilio.com/user/account
-            const string accountSid = "AC2006ab427bb0e190f055de1adac711fb";
-            const string apikey = "SK4084bf3d1502bcd7d135c8e936fa6870";
-            const string apiSecret = "XkS1l3GSlKU3dZJeNya2McvMaRYt3MuH";
-            const string credentialSid = "";
-            const string channelSid = "";
-
-            // List all members of a channel
-
-            var token = new AccessToken(accountSid, apikey, apiSecret) {Identity = "SimonMarkey"};
-
-            // Create an IP messaging grant for this token
-            var grant = new IpMessagingGrant
-            {
-                EndpointId = "EchelonChat:SimonMarkey:browser",
-                ServiceSid = "IS88ae154e76ce4907bd61282b27e295b5"
-            };
-
-            token.AddGrant(grant);
-
-            var client = new IpMessagingClient(accountSid, token.ToJWT());
-            MemberResult result = client.ListMembers(credentialSid, channelSid);
-            Console.WriteLine(result.Members.Count);
-        }
-    }
-}
+﻿//using System;
+//using NUnit.Framework;
+//using Twilio.Auth;
+//using Twilio.IpMessaging;
+//using Twilio.IpMessaging.Model;
+//
+//namespace Echelon.Tests.Twilio
+//{
+//    public class TwilioConnectionTests
+//    {
+//        [Test]
+//        [Ignore("Twilio issues")]
+//        public void Method_Scenario_Result()
+//        {
+//            // Find your Account Sid and Auth Token at twilio.com/user/account
+//            const string accountSid = "AC2006ab427bb0e190f055de1adac711fb";
+//            const string apikey = "SK4084bf3d1502bcd7d135c8e936fa6870";
+//            const string apiSecret = "XkS1l3GSlKU3dZJeNya2McvMaRYt3MuH";
+//            const string credentialSid = "";
+//            const string channelSid = "";
+//
+//            // List all members of a channel
+//
+//            var token = new AccessToken(accountSid, apikey, apiSecret) {Identity = "SimonMarkey"};
+//
+//            // Create an IP messaging grant for this token
+//            var grant = new IpMessagingGrant
+//            {
+//                EndpointId = "EchelonChat:SimonMarkey:browser",
+//                ServiceSid = "IS88ae154e76ce4907bd61282b27e295b5"
+//            };
+//
+//            token.AddGrant(grant);
+//
+////            var client = new IpMessagingClient(accountSid, token.ToJWT());
+////            MemberResult result = client.ListMembers(credentialSid, channelSid);
+////            Console.WriteLine(result.Members.Count);
+//        }
+//    }
+//}
